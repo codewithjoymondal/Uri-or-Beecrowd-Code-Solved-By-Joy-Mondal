f93dee115e37c866e7af5dfd73193cbe2bc5138b
codewithjoymondal <joymondal999.me@gmail.com>
2022-01-09 01:28:28 +0600

    init

---
+++ Uri-or-Beecrowd-Code-Solved-By-Joy-Mondal/1005.c
@@ -0,0 +1,12 @@
+//This Code Solved By Joy Mondal (codewithjoymonda) WUB 53A
+//If You Want to learn more code visite my youtube channel : Codewithjoy
+//https://www.youtube.com/c/CodeWithJoy
+
+#include <stdio.h>
+int main()
+{
+    float x,y;
+    scanf("%f %f", &x, &y);
+    printf("MEDIA = %.5f\n", (x*3.5+y*7.5)/(3.5+7.5));
+    return 0;
+}
