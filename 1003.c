0c35f6a4ad1a8f729405ef398c06089df6f592b6
codewithjoymondal <joymondal999.me@gmail.com>
2022-01-09 01:10:20 +0600

    init

---
+++ 1003.c
@@ -0,0 +1,14 @@
+//This Code Solved By Joy Mondal (codewithjoymonda) WUB 53A
+//If You Want to learn more code visite my youtube channel : Codewithjoy
+//https://www.youtube.com/c/CodeWithJoy
+#include <stdio.h>
+ 
+int main() {
+ 
+   int x,y;
+   scanf("%d",&x);
+   scanf("%d",&y);
+   printf("SOMA = %d\n",x+y);
+ 
+    return 0;
+}
---
+++ URI 1001.c
@@ -0,0 +1,12 @@
+//This Code Solved By Joy Mondal (codewithjoymonda) WUB 53A
+//If You Want to learn more code visite my youtube channel : Codewithjoy
+//https://www.youtube.com/c/CodeWithJoy
+#include<stdio.h>
+int main()
+{
+    int A,B,X;
+    scanf("%d %d",&A,&B);                                                                                                         
+    X = A + B;
+    printf("X = %d\n",X);
+    return 0;
+}
