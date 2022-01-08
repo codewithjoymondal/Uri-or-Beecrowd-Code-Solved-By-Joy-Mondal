ed3194ceb865162acc931158a86788bcb4ce76bd
codewithjoymondal <joymondal999.me@gmail.com>
2022-01-09 01:04:49 +0600

    init

---
+++ 1000.c
@@ -0,0 +1,12 @@
+//This Code Solved By Joy Mondal (codewithjoymonda) WUB 53A
+//If You Want to learn more code visite my youtube channel : Codewithjoy
+//https://www.youtube.com/c/CodeWithJoy
+
+#include <stdio.h>
+ 
+int main() {
+ 
+    printf("Hello World!\n");
+ 
+    return 0;
+}
