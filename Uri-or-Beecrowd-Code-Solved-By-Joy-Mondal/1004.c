d0915eaa46675cbffd1323deba625a630f068c8b
codewithjoymondal <joymondal999.me@gmail.com>
2022-01-09 01:23:51 +0600

    init

---
+++ Uri-or-Beecrowd-Code-Solved-By-Joy-Mondal/1004.c
@@ -0,0 +1,13 @@
+//This Code Solved By Joy Mondal (codewithjoymonda) WUB 53A
+//If You Want to learn more code visite my youtube channel : Codewithjoy
+//https://www.youtube.com/c/CodeWithJoy
+
+#include <stdio.h>
+int main()
+{
+     int x,y;
+     scanf("%d%d", &x, &y);
+     printf("PROD = %d\n", x * y);
+     return 0;
+
+} 
